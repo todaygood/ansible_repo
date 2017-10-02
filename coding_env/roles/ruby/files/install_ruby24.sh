a36778c657686038d1505454a61875ba0c23c9f1
Margin <jhu_com@163.com>
2017-10-02 09:11:04 +0800

    add ruby role

--- coding_env/roles/ruby/files/install_ruby24.sh
+++ coding_env/roles/ruby/files/install_ruby24.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-yum --enablerepo=centos-sclo-rh -y install rh-ruby24 
-scl enable rh-ruby24 bash 
+yum --enablerepo=centos-sclo-rh -y install rh-ruby24 rh-ruby24-ruby-devel  rh-ruby24-ruby-doc
+
+# commen the following line for ansible runing, so you need run it after finsishing ansible.
+#scl enable rh-ruby24 bash 
+
+############################################################### 
+#[root@mycentos7 coding_env]# ls /etc/scl/conf/rh-ruby2
+#rh-ruby22  rh-ruby24 
+##############################################################
